54e50c2f7a326f96e46cfb358b3a0df14d71a29b
Johan Roug <johanroug@johans-mbp.home>
2016-10-02 22:12:35 +0200

    no message

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log("asd2");
+class Person {
+  name: string;
+  private type: string;
+  protected age: number;
+
+  constructor(name: string, public userName: string) {
+    this.name = name;
+  }
+}
+
+const person = new Person("Johan", "johan");
+console.log(person);
+console.log(person.name);
